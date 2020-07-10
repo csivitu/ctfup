3ab2efd8a99b0cbe43acb3b94acbebfad4dbca16
Rohan Mukherjee <rohanbayern@gmail.com>
2020-07-10 16:44:18 +0530

    fix: diff command takes commitHash

--- src/command.ts
+++ src/command.ts
@@ -25,12 +25,15 @@ export async function runCommand(command: string, args: string[]) {
     });
 }
 
-export function diff(commitHash: string) {
+export function diff(commitHash: string): Promise<Array<string>> {
     return new Promise((resolve, reject) => {
-        exec(`git diff --dirstat=files,0 ${commitHash} | sed 's/^[ 0-9.]\\+% //g'`, (error, stdout) => {
+        exec(`git diff --dirstat=files,0 ${commitHash} | sed 's/^[ 0-9.]\\+% //g'`, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
                 return;
+            }
+            if (stderr) {
+                logger.error(stderr);
             }
             resolve(stdout.trim().split('\n').map((line) => line.trim().slice(0, -1)));
         });
